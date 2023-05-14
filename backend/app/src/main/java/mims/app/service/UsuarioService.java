84e4769dde081b5ddfff534027f1967acf5a04f1
Alen Galindo <alen.galindo@usach.cl>
2023-05-13 20:12:12 -0400

    [Alen Galindo] - fix: corregidos los metodos del servicio
    Ahora entregan tipo de dato correcto

--- backend/app/src/main/java/mims/app/service/UsuarioService.java
+++ backend/app/src/main/java/mims/app/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package mims.app.service;
 
 import mims.app.entity.UsuarioEntity;
+import mims.app.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,18 +19,32 @@ public class UsuarioService {
     }
 
     public ResponseEntity<UsuarioEntity> get_usuario_by_id(int id) {
-        return usuarioRepository.get_usuario_by_id(id);
+        return usuarioRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     public ResponseEntity<UsuarioEntity> save_usuario(UsuarioEntity usuario) {
-        return usuarioRepository.save_usuario(usuario);
+        return ResponseEntity.ok(usuarioRepository.save(usuario));
     }
 
     public ResponseEntity<UsuarioEntity> update_usuario(UsuarioEntity usuario, int id) {
-        return usuarioRepository.update_usuario(usuario, id);
+        return usuarioRepository.findById(id).map(usuario_data -> {
+            usuario_data.setNombre(usuario.getNombre());
+            usuario_data.setApellido(usuario.getApellido());
+            usuario_data.setRol(usuario.getRol());
+            usuario_data.setUpdated_at(usuario.getUpdated_at());
+            usuario_data.setUpdated_by(usuario.getUpdated_by());
+            usuario_data.setDeleted(usuario.getDeleted());
+            usuario_data.setRol(usuario.getRol());
+            UsuarioEntity usuario_updated = usuarioRepository.save(usuario_data);
+            return ResponseEntity.ok().body(usuario_updated);
+        }).orElse(ResponseEntity.notFound().build());
     }
 
     public ResponseEntity<UsuarioEntity> delete_usuario(int id) {
-        return usuarioRepository.delete_usuario(id);
+        return usuarioRepository.findById(id).map(usuario -> {
+            usuario.setDeleted(true);
+            UsuarioEntity usuario_deleted = usuarioRepository.save(usuario);
+            return ResponseEntity.ok().body(usuario_deleted);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
