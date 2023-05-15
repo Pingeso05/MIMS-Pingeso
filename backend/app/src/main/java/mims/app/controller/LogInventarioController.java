1ef84d33dfc7ec5f1d0445c051a7c47a5dbaa72b
Alen Galindo <alen.galindo@usach.cl>
2023-05-15 15:18:24 -0400

    [Alen Galindo] - ci: agregado controlador para log inventario
    Notese que por la naturaleza de la entidad, solo algunos metodos estan implementados.

---
+++ backend/app/src/main/java/mims/app/controller/LogInventarioController.java
@@ -0,0 +1,33 @@
+package mims.app.controller;
+
+import mims.app.entity.LogInventarioEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class LogInventarioController {
+
+    @Autowired
+    private LogInventarioService logInventarioService;
+
+    @GetMapping
+    public ResponseEntity<ArrayList<LogInventarioEntity>> get_all() {
+        return logInventarioService.get_all_not_deleted();
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<LogInventarioEntity> get_by_id(int id) {
+        return logInventarioService.get_by_id(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<LogInventarioEntity> save(@RequestBody LogInventarioEntity logInventario) {
+        return logInventarioService.save(logInventario);
+    }
+}
