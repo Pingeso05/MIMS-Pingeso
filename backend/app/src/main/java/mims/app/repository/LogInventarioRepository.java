ab5e06f976eb951c341b00ecc57b75e7e4970dac
Alen Galindo <alen.galindo@usach.cl>
2023-05-15 15:28:20 -0400

    [Alen Galindo] - add: agregado metodo para encontrar logs no borrados

--- backend/app/src/main/java/mims/app/repository/LogInventarioRepository.java
+++ backend/app/src/main/java/mims/app/repository/LogInventarioRepository.java
@@ -1,8 +1,10 @@
 package mims.app.repository;
 
+import mims.app.entity.LogInventarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public interface LogInventarioRepository extends JpaRepository<LogInventarioRepository, Integer> {
+public interface LogInventarioRepository extends JpaRepository<LogInventarioEntity, Integer> {
+    Iterable<LogInventarioEntity> findAllByDeletedFalse();
 }
