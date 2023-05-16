4c20eeb160802839682de8a140b8cb8293eb012f
Alen Galindo <alen.galindo@usach.cl>
2023-05-15 22:44:10 -0400

    [Alen Galindo] - ci: agregados modelos para tipo joya y locacion

---
+++ backend/app/src/main/java/mims/app/Model/DisplayLocacionModelInterface.java
@@ -0,0 +1,13 @@
+package mims.app.Model;
+
+@SuppressWarnings("unused")
+public interface DisplayLocacionModelInterface {
+    int getId();
+    String getNombre();
+    String getDescripcion();
+    String getDireccion();
+    String getCreatedAt();
+    String getUpdatedAt();
+    String getCreatedBy();
+    String getUpdatedBy();
+}
---
+++ backend/app/src/main/java/mims/app/Model/TipoJoyaModelInterface.java
@@ -0,0 +1,12 @@
+package mims.app.Model;
+
+@SuppressWarnings("unused")
+public interface TipoJoyaModelInterface {
+    int getId();
+    String getNombre();
+    String getMaterial();
+    String getCreatedAt();
+    String getUpdatedAt();
+    String getCreatedBy();
+    String getUpdatedBy();
+}
