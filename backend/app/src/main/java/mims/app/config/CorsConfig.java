c2fa9fe41c22b1882d972d042b073ae69c5999e0
Bastian Onetto <bastian.onetto@usach.cl>
2023-05-23 15:15:13 -0400

    [Bastian Onetto] fix: Se configura solucion para Cors

---
+++ backend/app/src/main/java/mims/app/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package mims.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://52.191.56.102:3000") // Agrega el origen no seguro permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
