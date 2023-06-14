69cb8e154ce5593cf71cf5158458356e1c66eb93
Bastian Onetto <bastian.onetto@usach.cl>
2023-06-13 22:49:18 -0400

    Hotfix App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Container from 'react-bootstrap/Container';
 import Layout from './Components/Layout';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Joya from './Pages/Joya';
+import AgregarProducto from './Pages/Agregar_Producto';
 import EditarProducto from './Pages/InventarioEditar';
 import Locaciones from './Pages/Locaciones';
 import AgregarLocacion from './Pages/Agregar_Locacion';
