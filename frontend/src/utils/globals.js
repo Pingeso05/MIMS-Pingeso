1df25b784fe0883fe2d4d724f14d488e5dd9b6f9
Bastian Onetto <bastian.onetto@usach.cl>
2023-06-20 23:03:03 -0400

    NA

--- frontend/src/utils/globals.js
+++ frontend/src/utils/globals.js
@@ -1,3 +1,3 @@
 const ruta_back = 'http://143.110.235.225:8080/';
-const ruta_front = 'http://localhost:3000/';
+const ruta_front = 'http://143.110.235.225:3000/';
 export { ruta_back, ruta_front };
