08400a1c63b7d1c71ab03d6bb4ecc4cbbc002f54
Cristobal Arias Barros <cristobal.arias@usach.cl>
2023-07-08 02:05:34 -0400

    Implementación de JWT

--- backend/app/src/main/java/mims/app/repository/UsuarioRepository.java
+++ backend/app/src/main/java/mims/app/repository/UsuarioRepository.java
@@ -6,8 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
     public ArrayList<UsuarioEntity> findAllUsuariosByDeletedFalse();
+
+    Optional<UsuarioEntity> findOneByEmail(String email);
 }
