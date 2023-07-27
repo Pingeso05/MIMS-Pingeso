139a69cdc5a8a91239bb23368dc8e21347e18214
Cristobal Arias Barros <cristobal.arias@usach.cl>
2023-07-26 23:05:19 -0400

    Arreglos en autenticacion

--- backend/app/src/main/java/mims/app/security/UserDetailServiceImpl.java
+++ backend/app/src/main/java/mims/app/security/UserDetailServiceImpl.java
@@ -14,12 +14,14 @@ public class UserDetailServiceImpl implements UserDetailsService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UsuarioEntity usuario = usuarioRepository
-                .findOneByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("El usuario con email " + email + "no existe."));
+        UsuarioEntity usuario = usuarioRepository.findOneByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario con email " + email + " no existe."));
+
+        if (usuario.getDeleted()) {
+            throw new UsernameNotFoundException("El usuario con email " + email + " ha sido eliminado.");
+        }
 
         return new UserDetailsImpl(usuario);
     }
